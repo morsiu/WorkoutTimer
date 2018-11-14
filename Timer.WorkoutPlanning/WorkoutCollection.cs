fd2918b9ce6bc81c903317de36be3150b61b68b0
Łukasz Mrozek <morskk@gmail.com>
2018-11-14 23:15:58 +0100

    Fix stale workound round.
    `WorkoutCollection` did not raise update of workout round when any if its items' property changed.

--- Timer.WorkoutPlanning/WorkoutCollection.cs
+++ Timer.WorkoutPlanning/WorkoutCollection.cs
@@ -23,25 +23,40 @@ namespace Timer.WorkoutPlanning
 
         protected override void ClearItems()
         {
+            foreach (var x in this)
+            {
+                x.PropertyChanged -= OnItemPropertyChanged;
+            }
             base.ClearItems();
             RaiseWorkoutRoundChange();
         }
 
         protected override void InsertItem(int index, Workout item)
         {
+            item.PropertyChanged += OnItemPropertyChanged;
             base.InsertItem(index, item);
             RaiseWorkoutRoundChange();
         }
 
         protected override void RemoveItem(int index)
         {
+            var item = Items[index];
+            item.PropertyChanged -= OnItemPropertyChanged;
             base.RemoveItem(index);
             RaiseWorkoutRoundChange();
         }
 
         protected override void SetItem(int index, Workout item)
         {
+            var oldItem = Items[index];
+            oldItem.PropertyChanged -= OnItemPropertyChanged;
+            item.PropertyChanged += OnItemPropertyChanged;
             base.SetItem(index, item);
+            RaiseWorkoutRoundChange();
+        }
+
+        private void OnItemPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
             RaiseWorkoutRoundChange();
         }
 
