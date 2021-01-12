2c520dab9bee94f9f2fe7378897ef9a0a3b32e6a
Lukasz Mrozek <lum@kmd.dk>
2021-01-12 21:08:50 +0100

    Move code for textual workout planning to separate project
    Extract code responsible for parsing text expressions to `WorkoutPlanExpression`,
    and place it in new project `WorkoutTimer.Planning.Textual`.

---
+++ WorkoutTimer.Planning.Textual/WorkoutPlanExpression.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using WorkoutTimer.Plans;
+
+namespace WorkoutTimer.Planning.Textual
+{
+    public sealed class WorkoutPlanExpression
+    {
+        private readonly string _expression;
+
+        public WorkoutPlanExpression(string expression)
+        {
+            _expression = expression;
+        }
+
+        public WorkoutPlan ToWorkoutPlan(WorkoutPlan result)
+        {
+            var parts = _expression.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            using var part = parts.AsEnumerable().GetEnumerator();
+            var previousAction = default(Func<WorkoutPlan, WorkoutPlan>);
+            while (part.MoveNext())
+            {
+                if (Value(part.Current) is { } value
+                    && part.MoveNext())
+                {
+                    if (Type(part.Current) is { } type)
+                    {
+                        if (Action(value, type, previousAction) is { } action)
+                        {
+                            result = action(result);
+                            previousAction = action;
+                            continue;
+                        }
+                        return result;
+                    }
+                    return result;
+                }
+                return result;
+
+                static Func<WorkoutPlan, WorkoutPlan> Action(
+                    int value,
+                    FieldType type,
+                    Func<WorkoutPlan, WorkoutPlan> previousAction)
+                {
+                    if (Duration.TryFromSeconds(value) is { } duration)
+                        switch (type)
+                        {
+                            case FieldType.Break:
+                                return x => x.AddBreak(duration);
+                            case FieldType.Exercise:
+                                return x => x.AddExercise(duration);
+                            case FieldType.Countdown:
+                                return x => x.WithCountdown(duration);
+                        }
+                    if (value == 0 && type == FieldType.Exercise) return x => x.AddExercise();
+                    if (Count.TryFromNumber(value) is { } count)
+                        switch (type)
+                        {
+                            case FieldType.Round:
+                                return x => x.WithRound(count);
+                            case FieldType.Repeat when previousAction != null:
+                                return x => Enumerable.Repeat(previousAction, count - 1)
+                                    .Aggregate(x, (a, b) => b(a));
+                        }
+                    return null;
+                }
+
+                static int? Value(string input)
+                {
+                    return int.TryParse(input, out var number)
+                        ? number
+                        : default;
+                }
+
+                static FieldType? Type(string input)
+                {
+                    return input.ToLower() switch
+                    {
+                        "e" => FieldType.Exercise,
+                        "b" => FieldType.Break,
+                        "w" => FieldType.Countdown,
+                        "r" => FieldType.Round,
+                        "*" => FieldType.Repeat,
+                        _ => null
+                    };
+                }
+            }
+
+            return result;
+        }
+
+        private enum FieldType
+        {
+            Break,
+            Exercise,
+            Countdown,
+            Round,
+            Repeat
+        }
+    }
+}
--- WorkoutTimer.Planning/WorkoutExpression.cs
+++ WorkoutTimer.Planning/WorkoutExpression.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Linq;
+using WorkoutTimer.Planning.Textual;
 using WorkoutTimer.Plans;
 
 namespace WorkoutTimer.Planning
@@ -9,103 +10,7 @@ namespace WorkoutTimer.Planning
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private enum FieldType
-        {
-            Break,
-            Exercise,
-            Countdown,
-            Round,
-            Repeat
-        }
-
-        public Func<WorkoutPlan, WorkoutPlan> WorkoutPlan
-        {
-            get
-            {
-                return result =>
-                {
-                    var parts = Value.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                    using var part = parts.AsEnumerable().GetEnumerator();
-                    var previousAction = default(Func<WorkoutPlan, WorkoutPlan>);
-                    while (part.MoveNext())
-                    {
-                        if (Value(part.Current) is { } value
-                            && part.MoveNext())
-                        {
-                            if (Type(part.Current) is { } type)
-                            {
-                                if (Action(value, type, previousAction) is { } action)
-                                {
-                                    result = action(result);
-                                    previousAction = action;
-                                    continue;
-                                }
-                                return result;
-                            }
-                            return result;
-                        }
-                        return result;
-
-                        static Func<WorkoutPlan, WorkoutPlan> Action(
-                            int value,
-                            FieldType type,
-                            Func<WorkoutPlan, WorkoutPlan> previousAction)
-                        {
-                            if (Duration.TryFromSeconds(value) is { } duration)
-                            {
-                                switch (type)
-                                {
-                                    case FieldType.Break:
-                                        return x => x.AddBreak(duration);
-                                    case FieldType.Exercise:
-                                        return x => x.AddExercise(duration);
-                                    case FieldType.Countdown:
-                                        return x => x.WithCountdown(duration);
-                                }
-                            }
-                            if (value == 0 && type == FieldType.Exercise)
-                            {
-                                return x => x.AddExercise();
-                            }
-                            if (Count.TryFromNumber(value) is { } count)
-                            {
-                                switch (type)
-                                {
-                                    case FieldType.Round:
-                                        return x => x.WithRound(count);
-                                    case FieldType.Repeat when previousAction != null:
-                                        return x => Enumerable.Repeat(previousAction, count - 1)
-                                            .Aggregate(x, (a, b) => b(a));
-                                }
-                            }
-                            return null;
-                        }
-
-                        static int? Value(string input)
-                        {
-                            return int.TryParse(input, out var number)
-                                ? number
-                                : default;
-                        }
-
-                        static FieldType? Type(string input)
-                        {
-                            return input.ToLower() switch
-                            {
-                                "e" => FieldType.Exercise,
-                                "b" => FieldType.Break,
-                                "w" => FieldType.Countdown,
-                                "r" => FieldType.Round,
-                                "*" => FieldType.Repeat,
-                                _ => null
-                            };
-                        }
-                    }
-
-                    return result;
-                };
-            }
-        }
+        public Func<WorkoutPlan, WorkoutPlan> WorkoutPlan => new WorkoutPlanExpression(Value).ToWorkoutPlan;
 
         public string ActualValue
         {
