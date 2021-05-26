71c888d6db402eb1f489e620f6ebc06fce688947
Lukasz Mrozek <lum@kmd.dk>
2021-05-26 22:02:00 +0200

    Encapsulate list of workouts in control

---
+++ WorkoutTimer.Tracking.Visual/WorkoutCollection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WorkoutTimer.Tracking.Visual
+{
+    [TemplatePart(Name = "PART_Items", Type = typeof(ItemsControl))]
+    public sealed class WorkoutCollection : Control
+    {
+        public static DependencyProperty WorkoutsProperty =
+            DependencyProperty.Register(nameof(Workouts), typeof(IEnumerable), typeof(WorkoutCollection));
+
+        public IEnumerable Workouts
+        {
+            get => (IEnumerable)GetValue(WorkoutsProperty);
+            set => SetValue(WorkoutsProperty, value);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            if (Template.FindName("PART_Items", this) is ItemsControl itemsPart)
+            {
+                BindingOperations.SetBinding(
+                    itemsPart,
+                    ItemsControl.ItemsSourceProperty,
+                    new Binding(nameof(Workouts)) { RelativeSource = RelativeSource.TemplatedParent });
+            }
+        }
+    }
+}
